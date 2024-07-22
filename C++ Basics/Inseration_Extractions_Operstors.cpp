9f395bdb05768b4052d9f9c51bc3fefb95512454
Atharva Kote <atharvkote3@gmail.com>
2024-07-22 22:26:04 +0530

    Create Inseration_Extractions_Operstors.cpp

---
+++ C++ Basics/Inseration_Extractions_Operstors.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void main(){
+  
+  int a,b;
+  cout<<"Enter Two Numbers";
+  cin>>a>>b;
+  cout<<"Value of a is">>a;
+  cout<<"Value of b is">>b;
+  
+}
