778b19c9f775a5a3c57c5aa9a40e57ab8ef10c00
Atharva Kote <atharvkote3@gmail.com>
2024-07-22 23:08:46 +0530

    Update 01_Insertion_Operator.cpp

--- C++ Basics/Operators/01_Insertion_Operator.cpp
+++ C++ Basics/Operators/01_Insertion_Operator.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main() {
+
+    // Print a Prompt Message
+    cout << "Hello World!";
+
+   // Print On New Line
+    cout << "Hello World!" << "\n";
+    cout << "I am learning C++";
   
+  
+    // Printing on NewLine using endl
+    cout << "Hello World!" << endl;
+    cout << "I am learning C++";
+
+    // Printing Values 
     int a=10;
     int b=20;
   
