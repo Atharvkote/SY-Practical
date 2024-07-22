199da36be0de1f14482655980253af3dd32e72d2
Atharva Kote <atharvkote3@gmail.com>
2024-07-22 22:42:35 +0530

    Create Insertion_Operator.cpp

---
+++ C++ Basics/Operators/Insertion_Operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    int a=10;
+    int b=20;
+  
+    cout << "Value of a is " << a << endl;
+    cout << "Value of b is " << b << endl;
+    return 0;
+}
