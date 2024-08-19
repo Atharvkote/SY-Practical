27568d7807e4fe2e42daa66a115a96b4d6417cb9
Atharva Kote <atharvkote3@gmail.com>
2024-08-19 10:03:07 +0530

    Update 02-BookStoreClass.cpp

--- OOP-Practical/02-BookStoreClass/02-BookStoreClass.cpp
+++ OOP-Practical/02-BookStoreClass/02-BookStoreClass.cpp
@@ -1,1 +1,26 @@
 
+#include<iostream>
+#include<string>
+
+class BookShop{
+    
+   private:
+               string book_name;
+               string book_author;
+               string book_name;
+               string book_publisher;
+               int book_price;
+               int book_copies;
+        
+   public: 
+              BookShop(){
+                   
+                  }
+   
+   
+    };
+   
+  int main(){
+     
+      return 0; 
+  }
