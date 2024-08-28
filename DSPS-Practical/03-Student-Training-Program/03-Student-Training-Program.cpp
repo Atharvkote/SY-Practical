a211a543d03eab98f9fe8b25db5661052c9b475b
Atharva Kote <atharvkote3@gmail.com>
2024-08-28 10:02:02 +0530

    Update 03-Student-Training-Program.cpp

--- DSPS-Practical/03-Student-Training-Program/03-Student-Training-Program.cpp
+++ DSPS-Practical/03-Student-Training-Program/03-Student-Training-Program.cpp
@@ -3,165 +3,159 @@
 
 using namespace std;
 
-class Student{
-      private :
-            string name;
-            int roll_number;
-            string _class;
-      public:
-            Student(){
-                strcpy(name,"No Record");
-                roll_number = 0;
-                strcpy(_class,"No Class Alloted");
-                
+class Student {
+private:
+    string name;
+    int roll_number;
+    string _class;
+
+public:
+    // Default constructor
+    Student() : name("No Record"), roll_number(0), _class("No Class Allotted") {}
+
+    // Parameterized constructor
+    Student(string _name, int _roll_number, string __class)
+        : name(_name), roll_number(_roll_number), _class(__class) {}
+
+    // Function to print all student records
+    static void print_shelf(Student division[], int class_strength) {
+        for (int i = 0; i < class_strength; i++) {
+            cout << "Name: " << division[i].name
+                 << " | Roll Number: " << division[i].roll_number
+                 << " | Class: " << division[i]._class << endl;
+        }
+    }
+
+    // Function to check if the array is sorted
+    static bool isSorted(Student division[], int class_strength) {
+        for (int j = 0; j < class_strength - 1; j++) {
+            if (!(division[j].roll_number < division[j + 1].roll_number)) {
+                return false;
             }
+        }
+        return true;
+    }
 
-            Student(string _name, int _roll_number, string __class){
-                 strcpy(name,_name);
-                 roll_number = _roll_number;
-                 strcpy(_class,__class);
-            }
-
-            static void print_shelf(Student divison[], int class_strength) {
-                  for (int i = 0; i < class_strength; i++) {
-                     cout << "          " << divison[i].name << "                        " << divison[i].roll_number <<"         " << divison[i]._class << endl;
-                  }
-            
-            }
-
-            static bool isSorted(Student divison[]){
-
-                for (int j = 0; j < class_strength +1 ; j++) {
-                      if(!(divison[j].roll_number < divison[j+1].roll_number)){
-                           return false;
-                     }
+    // Selection sort algorithm
+    static void selectionSort(Student division[], int class_strength) {
+        for (int i = 0; i < class_strength - 1; i++) {
+            for (int j = i + 1; j < class_strength; j++) {
+                if (division[i].roll_number > division[j].roll_number) {
+                    swap(division[i], division[j]);
                 }
-              return true;
             }
+        }
+    }
 
-            static void selectionSort(Student divison[], int class_strength){
-
-                 for (int i = 0; i < class_strength - 1; i++) {
-                      for (int j = i + 1; j < class_strength; j++) {
-                
-                           if (divison[i].roll_number < divison[j].roll_number) {
-             
-                                    Student temp =divison[i];
-                                    divison[i] =divison[j];
-                                    divison[j] = temp;
-                            }
-                       }
-                   }
-               }
-            static int linearSearch(Student divison[],int class_strength ,int target){
-                 
-                 for(int i = 0; i < class_strength ; i++){
-                    if(divison[i].roll_number == target){
-                        return i;
-                    }
-                 }
-                 return -1;
+    // Linear search algorithm
+    static int linearSearch(Student division[], int class_strength, int target) {
+        for (int i = 0; i < class_strength; i++) {
+            if (division[i].roll_number == target) {
+                return i;
             }
+        }
+        return -1;
+    }
 
-            static int binarySearch(Student divison[],int class_strength ,int target){
+    // Binary search algorithm
+    static int binarySearch(Student division[], int class_strength, int target) {
+        if (!isSorted(division, class_strength)) {
+            cout << "You entered an unsorted array. Sorting the array...\n";
+            selectionSort(division, class_strength);
+        }
 
-                if(!isSorted){
-                    cout << "You Entered A Unsorted Array [ Sorting Given ARRAY ]\n"
-                    selectionSort(Student divison[], class_strength);
-                }
-                int low = 0;
-                int high = class_strength -1;
-
-                while(low < high){
-                    
-                    int mid = low + ( high - low ) /2;
-
-                    if(divison[mid].roll_number == target){
-                        return mid;
-                    }else if(divison[i].roll_number > target){
-                        low = mid;
-                    }else{
-                        high = mid;
-                    }
-                  }
-                    return -1;
-                }
-            
-            void display(){
-
+        int low = 0, high = class_strength - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (division[mid].roll_number == target) {
+                return mid;
+            } else if (division[mid].roll_number < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
+        }
+        return -1;
+    }
 
+    // Function to display student details
+    void display() {
+        cout << "Name: " << name << " | Roll Number: " << roll_number << " | Class: " << _class << endl;
+    }
+};
 
-   };
-
-   int main(){
-         Student divison[10];
-         int class_strength, choice;
+int main() {
+    Student division[10];
+    int class_strength, choice;
 
     cout << "Enter Number of Entries: ";
     cin >> class_strength;
 
+    string _name, __class;
+    int _roll_number;
+
     for (int i = 0; i < class_strength; i++) {
-        cout<<"Enter Details of Book - "<<i+1<<" : "<<endl;
-        cout << "  • Enter Roll Number : ";
-        cin >>  _roll_number;
-        cout << "  • Enter Name : ";
+        cout << "Enter Details of Student - " << i + 1 << " : " << endl;
+        cout << "  • Enter Roll Number: ";
+        cin >> _roll_number;
+        cout << "  • Enter Name: ";
         cin >> _name;
         cout << "  • Enter Class: ";
         cin >> __class;
 
-        divison[i] = Student(_name, _roll_number,__class);
+        division[i] = Student(_name, _roll_number, __class);
     }
 
     do {
-       cout << "\nOperation Menu:" << endl;
-        cout << "1. Sort with respect to Roll Number" << endl;
-        cout << "2. Display" << endl;       
-        cout << "3. Check Student Present or NOT ( Optimized ) " << endl;
-        cout << "4. Check Student Present or NOT ( UnOptimized ) "<<endl;
+        cout << "\nOperation Menu:" << endl;
+        cout << "1. Sort by Roll Number" << endl;
+        cout << "2. Display All Students" << endl;
+        cout << "3. Check Student Presence (Optimized)" << endl;
+        cout << "4. Check Student Presence (Unoptimized)" << endl;
         cout << "5. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
-            case 1: {
-                Student :: selectionSort(divison);
+            case 1:
+                Student::selectionSort(division, class_strength);
                 break;
-            }
-            case 2: {
-                 for (int i = 0; i < class_strength; i++) {
-
-                         cout<<"Enter Details of Book - "<<i+1<<" : "<<endl;
-                         cout << "  • Enter Roll Number : ";
-                         cin >>  _roll_number;
-                         cout << "  • Enter Name : ";
-                         cin >> _name;
-                         cout << "  • Enter Class: ";
-                         cin >> __class;
-                 }
+            case 2:
+                Student::print_shelf(division, class_strength);
                 break;
-            }
             case 3: {
                 int target;
-                cout<<"Enter Roll Number of Student :";
-                cin >>target;
-                cout << "Searching Using Binary Search ......" << endl;     
-                Student :: binarySearch(divison, target,class_strength);
+                cout << "Enter Roll Number of Student: ";
+                cin >> target;
+                cout << "Searching using Binary Search..." << endl;
+                int index = Student::binarySearch(division, class_strength, target);
+                if (index != -1) {
+                    division[index].display();
+                } else {
+                    cout << "Student not found." << endl;
+                }
                 break;
             }
-            case 4:
-                 int target;
-                 cout<<"Enter Roll Number of Student :";
-                 cin >>target;
-                 cout << "Searching Using Linear Search ......" << endl;                 
-                 Student :: linearSearch(divison, target, class_strength);
-                 break;
+            case 4: {
+                int target;
+                cout << "Enter Roll Number of Student: ";
+                cin >> target;
+                cout << "Searching using Linear Search..." << endl;
+                int index = Student::linearSearch(division, class_strength, target);
+                if (index != -1) {
+                    division[index].display();
+                } else {
+                    cout << "Student not found." << endl;
+                }
+                break;
+            }
             case 5:
-            	 break;
+                cout << "Exiting..." << endl;
+                break;
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
     } while (choice != 5);
 
     return 0;
-   }
+}
