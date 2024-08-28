d380a0b9248695660143753863665348d10a39d3
Atharva Kote <atharvkote3@gmail.com>
2024-08-28 21:34:50 +0530

    Update 03-Student-Training-Program.cpp

--- DSPS-Practical/03-Student-Training-Program/03-Student-Training-Program.cpp
+++ DSPS-Practical/03-Student-Training-Program/03-Student-Training-Program.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
@@ -8,25 +9,90 @@ private:
     string name;
     int roll_number;
     string _class;
+    float feedback;
 
 public:
     // Default constructor
-    Student() : name("No Record"), roll_number(0), _class("No Class Allotted") {}
-
-    // Parameterized constructor
-    Student(string _name, int _roll_number, string __class)
-        : name(_name), roll_number(_roll_number), _class(__class) {}
-
-    // Function to print all student records
-    static void print_shelf(Student division[], int class_strength) {
-        for (int i = 0; i < class_strength; i++) {
-            cout << "Name: " << division[i].name
-                 << " | Roll Number: " << division[i].roll_number
-                 << " | Class: " << division[i]._class << endl;
-        }
+    Student() {
+        name = "No Record";
+        roll_number = 0;
+        _class = "No Class Allotted";
+        feedback = 0.0;
     }
 
-    // Function to check if the array is sorted
+    float getFeedBack() {
+        int Feedback[5];
+        cout << "    1. Did You understand the Purpose of the Training Program at start of the event?\n  \t• ( Press 1 [ YES ]  Press  0 [NO] ) : ";
+        cin >> Feedback[0];
+        cout << "    2. Did the Training Meet your Expectations?\n  \t• ( Press 1 [ For 8/10 or more ]  Press  0 [ 7/10 or less] ) :  ";
+        cin >> Feedback[1];
+        cout << "    3. Overall, how would you rate the training instructor?\n  \t• ( Press 1 [ YES ]  Press  0 [NO] ) :   ";
+        cin >> Feedback[2];
+        cout << "    4. Was the Training program well-organized and easy to follow?\n  \t• ( Press 1 [ YES ]  Press  0 [NO] ) :  ";
+        cin >> Feedback[3];
+        cout << "    5. Do you feel you were given enough time and resources to complete the training?\n  \t• ( Press 1 [ YES ]  Press  0 [NO] ) :  ";
+        cin >> Feedback[4];
+
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            if (Feedback[i] == 1) {
+                count++;
+            }
+        }
+
+        return (count / 5.0) * 100;
+    }
+
+    // Parameterized constructor
+    Student(string _name, int _roll_number, string __class) {
+        name = _name;
+        roll_number = _roll_number;
+        _class = __class;
+        feedback = getFeedBack();
+    }
+    
+    static void print_row_header(ostream& os, const string& to, const string& t1, const string& t2, const string& t3, string t4, const string& t5) {
+        os << "| " << setw(4) << to
+           << " | " << setw(10) << t1
+           << " | " << setw(10) << t2
+           << " | " << setw(15) << t3
+           << " | " << setw(7) << t4
+           << " | " << setw(7) << t5
+           << " |\n";
+    }
+
+
+    
+    static void print_row(ostream& os, const string& to, const string& t1, const string& t2, const string& t3, float t4, const string& t5) {
+        os << "| " << setw(4) << to
+           << " | " << setw(10) << t1
+           << " | " << setw(10) << t2
+           << " | " << setw(15) << t3
+           << " | " << setw(7) << fixed << setprecision(2) << t4
+           << " | " << setw(7) << t5
+           << " |\n";
+    }
+
+    
+    static void print_border(ostream& os) {
+        os << "+------+------------+------------+---------------+---------+-----------+\n";
+    }
+
+    
+    static void print(Student division[], int class_strength) {
+        print_border(cout);
+        print_row_header(cout, "No.", "Name", "Roll No.", "Class", "Rating", "Status");
+        print_border(cout);
+
+        for (int i = 0; i < class_strength; i++) {
+            string status = division[i].feedback > 60 ? "+ve" : "-ve";
+            print_row(cout, to_string(i + 1), division[i].name, to_string(division[i].roll_number), division[i]._class, division[i].feedback, status);
+        }
+
+        print_border(cout);
+    }
+
+    
     static bool isSorted(Student division[], int class_strength) {
         for (int j = 0; j < class_strength - 1; j++) {
             if (!(division[j].roll_number < division[j + 1].roll_number)) {
@@ -36,33 +102,50 @@ public:
         return true;
     }
 
-    // Selection sort algorithm
     static void selectionSort(Student division[], int class_strength) {
         for (int i = 0; i < class_strength - 1; i++) {
             for (int j = i + 1; j < class_strength; j++) {
                 if (division[i].roll_number > division[j].roll_number) {
-                    swap(division[i], division[j]);
+                    Student temp = division[i];
+                    division[i] = division[j];
+                    division[j] = temp;
                 }
             }
         }
     }
 
-    // Linear search algorithm
-    static int linearSearch(Student division[], int class_strength, int target) {
+   
+    static float feedbackSummary(Student division[], int class_strength) {
+        float total = 0;
         for (int i = 0; i < class_strength; i++) {
-            if (division[i].roll_number == target) {
+            total += division[i].feedback;
+        }
+
+        return total / class_strength;
+    }
+
+    
+    static int linearSearch(Student division[], int class_strength, int target,string target_class) {
+        for (int i = 0; i < class_strength; i++) {
+            if (division[i].roll_number == target && division[i]._class == target_class)  {
                 return i;
             }
         }
         return -1;
     }
 
-    // Binary search algorithm
-    static int binarySearch(Student division[], int class_strength, int target) {
+    
+    static int binarySearch(Student division[], int class_strength) {
         if (!isSorted(division, class_strength)) {
             cout << "You entered an unsorted array. Sorting the array...\n";
             selectionSort(division, class_strength);
         }
+
+        int target;
+        cout << "Enter Roll Number of Student: ";
+        cin >> target;
+
+        cout << "Searching using Binary Search..." << endl;
 
         int low = 0, high = class_strength - 1;
         while (low <= high) {
@@ -78,9 +161,14 @@ public:
         return -1;
     }
 
-    // Function to display student details
-    void display() {
-        cout << "Name: " << name << " | Roll Number: " << roll_number << " | Class: " << _class << endl;
+    
+    void display(int i) {
+        print_border(cout);
+        print_row_header(cout, "No.", "Name", "Roll No.", "Class", "Rating", "Status");
+        print_border(cout);
+        string status = feedback > 60 ? "+ve" : "-ve";
+        print_row(cout, to_string(i + 1),name, to_string(roll_number),_class,feedback, status);
+        print_border(cout);
     }
 };
 
@@ -88,7 +176,7 @@ int main() {
     Student division[10];
     int class_strength, choice;
 
-    cout << "Enter Number of Entries: ";
+    cout << "Enter Number of Students Attended the Program : ";
     cin >> class_strength;
 
     string _name, __class;
@@ -109,10 +197,11 @@ int main() {
     do {
         cout << "\nOperation Menu:" << endl;
         cout << "1. Sort by Roll Number" << endl;
-        cout << "2. Display All Students" << endl;
+        cout << "2. Display All Students FEEDBACK" << endl;
         cout << "3. Check Student Presence (Optimized)" << endl;
         cout << "4. Check Student Presence (Unoptimized)" << endl;
-        cout << "5. Exit" << endl;
+        cout << "5. Get FEEDBACK Summary" << endl;
+        cout << "6. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -121,41 +210,60 @@ int main() {
                 Student::selectionSort(division, class_strength);
                 break;
             case 2:
-                Student::print_shelf(division, class_strength);
+                Student::print(division, class_strength);
                 break;
             case 3: {
-                int target;
-                cout << "Enter Roll Number of Student: ";
-                cin >> target;
-                cout << "Searching using Binary Search..." << endl;
-                int index = Student::binarySearch(division, class_strength, target);
+                int index = Student::binarySearch(division, class_strength);
                 if (index != -1) {
-                    division[index].display();
+                    cout << "Student was Present for the Training Program\nStudent Details:\n\n";
+                    division[index].display(index);
+                    cout << "\n\n";
                 } else {
-                    cout << "Student not found." << endl;
+                    cout << "Student was NOT Present for the Training Program" << endl;
                 }
                 break;
             }
             case 4: {
                 int target;
+                string target_class;
                 cout << "Enter Roll Number of Student: ";
                 cin >> target;
+                cout << "Enter Class of Student: ";
+                cin >> target_class;
+
+                int index = Student::linearSearch(division, class_strength, target, target_class);
+
                 cout << "Searching using Linear Search..." << endl;
-                int index = Student::linearSearch(division, class_strength, target);
+
                 if (index != -1) {
-                    division[index].display();
+                    cout << "Student was Present for the Training Program\nStudent Details:\n";
+                    division[index].display(index);
                 } else {
-                    cout << "Student not found." << endl;
+                    cout << "Student was NOT Present for the Training Program" << endl;
                 }
                 break;
             }
-            case 5:
+            case 5: {
+                Student::print(division, class_strength);
+                cout << "\n\n";
+                float Summary;
+                Summary = Student::feedbackSummary(division, class_strength);
+                if (Summary > 60) {
+                    cout << "\t\t [ RESPONSE IS SATISFACTORY !!!! ]\n";
+                    cout << "\t\tOverall RATING:  [ " << fixed << setprecision(2) << Summary <<"  ]"<< endl;
+                } else {
+                    cout << "\t\t[ RESPONSE IS NOT SATISFACTORY !!!! ]\n";
+                    cout << "\t\tOverall RATING: [  " << fixed << setprecision(2) << Summary<<" ]" << endl;
+                }
+                break;
+            }
+            case 6:
                 cout << "Exiting..." << endl;
                 break;
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
-    } while (choice != 5);
+    } while (choice != 6);
 
     return 0;
 }
